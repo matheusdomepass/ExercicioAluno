73d42a1fe72813d638af799e0d62f8bc5ade54d4
matheus.domeneghetti <matheus.domeneghetti@RPOLOG0046>
2024-06-18 10:14:00 -0300

    Adicionar arquivos de projeto.

---
+++ Aluno.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioAluno
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+        
+
+        public double Media()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public bool Aprovado()
+        {
+            if (Media() >= 60.00)
+            {
+                return true;
+            }
+            else { 
+                return false;
+            }  
+        }
+
+        public double NotaRestante()
+        {
+            if (Aprovado())
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 60.0 - Media();
+            }
+        }
+    }
+}
